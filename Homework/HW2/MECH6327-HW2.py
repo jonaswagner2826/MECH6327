9f9be1b60b8700f82e6f356baf919e7e533691a9
jonaswagner2826 <47377464+jonaswagner2826@users.noreply.github.com>
2021-03-02 23:41:41 -0600

    HW2:Progress made...

--- Homework/HW2/MECH6327-HW2.py
+++ Homework/HW2/MECH6327-HW2.py
@@ -9,25 +9,33 @@ Created on Mon Mar  1 23:27:18 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sp
 
 # Problem 2.7
 a = np.array([[2, 4]]).T
 b = np.array([[-3, 7]]).T
 
-c = (a - b)
-d = (c.T.dot(a) + c.T.dot(b)) / 2
+c = (b - a)
+d = (c.T.dot(a + b)) / 2
+
+m = (a+b)/2
 
 # Axis Setup
 fig, ax = plt.subplots()
-ax.plot([-2, 5], [0, 0], 'k')
-ax.plot([0, 0], [-4, 8], 'k')
+ax.set_aspect('equal', adjustable='datalim')
+ax.plot([-5, 5], [0, 0], 'k')
+ax.plot([0, 0], [-1, 10], 'k')
 
 # Points a and b
 x, y = np.array([a.T, b.T]).T
 ax.scatter(x, y)
 
+
 # Boundary
-X = (np.linspace(-2, 2) * np.ones((2,1))).T
-x, y = np.array(- 1 * c.T / X + d).T
-ax.plot(x,y)
+x = np.linspace(-4,2)
+y =  (d - c[0] * x) / c[1]
+ax.plot(x,y.T,'r')
+
+plt.savefig('fig/pblm2_7.png')
+
 
